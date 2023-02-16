accd19fbba2a553854ff0dd9bd2f2e8e9d1901e8
Dluxed <118645102+Dluxed@users.noreply.github.com>
2023-02-15 22:08:01 -0700

    Iniciado traductor corregido

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Translate from "./components/Translate";
+function App() {
+  return (
+    <nav>
+
+    </nav>,
+    <>
+      <Translate />
+    </>
+  );
+}
+
+export default App;
